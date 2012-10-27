0ff54019690d49d4d5a0eb511e396c1dedba8152
xprazak2 <324326@mail.muni.cz>
2012-10-27 17:12:29 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AvidReader::Application.config.secret_token = 'cda7b0c3dce861668729943f54ec1925ff5e2523d65a9cd082b12aefe580019040b4e07178d9721b41078d902208aa2c690d69a8830c9d570397438dcb314025'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AvidReader::Application.config.session_store :cookie_store, key: '_avid_reader_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AvidReader::Application.config.session_store :active_record_store
